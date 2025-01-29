f080f2fdcf53d609a3289ec66dab3342bbbdb7d4
mohamed <mohamedsamirtkd@gmail.com>
2025-01-29 11:00:09 +0200

    Added YOLOv9 support, And updated Readme, image_inference.cpp, video_inference.cpp to support it

--- models/export_onnx.py
+++ models/export_onnx.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-# Load the YOLOv11n model
-model = YOLO("yolo11l.pt")
+# Load the YOLOv9s model
+model = YOLO("yolov9s.pt")
 
 # Export the model to ONNX format
-model.export(format="onnx")
+model.export(format = "onnx")
