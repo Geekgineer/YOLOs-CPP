5a368b85ed56663c8e8dcba38cd519be687fff3a
Mohamed Essam <imessam1998@gmail.com>
2025-09-01 22:34:46 +0300

    DEV: ultralytics pipeline
    Former-commit-id: f94add52875ff5ff92d96915eff9008de10aeea1

---
+++ test/test_inference_ultralytics.py
@@ -0,0 +1,180 @@
+import sys
+import os
+import shutil
+import json
+
+from tqdm.auto import tqdm
+from ultralytics import YOLO
+
+def validate_paths(paths : dict) -> bool:
+
+    print("Validating paths...")
+
+    data_path = paths.get("data", None)
+    if data_path is None or not os.path.exists(data_path):
+        print(f"Data path '{data_path}' does not exist.")
+        return False
+
+    images_path = os.path.join(data_path, "images")
+    if not os.path.exists(images_path):
+        print(f"Images path '{images_path}' does not exist.")
+        return False
+    
+    images_files = os.listdir(images_path)
+    if len(images_files) == 0:
+        print(f"No images found in '{images_path}'.")
+        return False
+
+    weights_path = paths.get("weights", None)
+    if weights_path is None or not os.path.exists(weights_path):
+        print(f"Weights path '{weights_path}' does not exist.")
+        return False
+
+    return True
+
+def main():
+
+    data_path = "data"
+    images_path = os.path.join(data_path, "images")
+    weights_path = "models"
+    
+    paths_to_validate = {
+        "data": data_path,
+        "images": images_path,
+        "weights": weights_path
+    }
+
+    if not validate_paths(paths_to_validate):
+        print("Path validation failed, exiting.")
+        sys.exit(1)
+
+    results_path = "results"
+    if os.path.exists(results_path):
+        print(f"Results path '{results_path}' already exists, removing it and creating a new one.")
+        shutil.rmtree(results_path)
+    os.makedirs(results_path)
+
+    inference_config = {
+            "conf": 0.25,
+            "iou": 0.25
+    }
+    inference_config_path = "inference_config.json"
+
+    if os.path.exists(inference_config_path):
+
+        with open(inference_config_path, "r") as f:
+
+            inference_config_loaded = {}
+
+            try:
+                inference_config_loaded = json.load(f)
+
+                if "conf" not in inference_config_loaded or "iou" not in inference_config_loaded:
+                    print(f"Inference configuration file '{inference_config_path}' is missing required keys, using default configuration : {inference_config}.")
+                else:
+                    inference_config = inference_config_loaded
+                    print(f"Loaded inference configuration from '{inference_config_path}' : {inference_config} .")
+
+            except json.JSONDecodeError as e:
+                print(f"Error loading inference configuration file '{inference_config_path}': {e}, using default configuration : {inference_config}.")
+
+    output_results_json = os.path.join(results_path, "results_ultralytics.json")
+
+    models = [
+        "yolov5nu",
+        "yolov6n",
+        "yolov8n",
+        "yolov9t",
+        "yolov10n",
+        "yolo11n",
+        "yolo12n"
+    ]
+
+    results_dict = {}
+
+    for model_name in tqdm(models, desc="Models to test", unit="model"):
+
+        print(f"Running inference for model: {model_name}")
+
+        model_weights = os.path.join(weights_path, f"{model_name}.onnx")
+        if not os.path.exists(model_weights):
+            print(f"Model weights '{model_weights}' do not exist, skipping.")
+            continue
+
+        if model_name not in results_dict:
+            results_dict[model_name] = {
+                "weights_path": model_weights,
+                "task": "detect",
+            }
+
+        model = YOLO(
+            model = model_weights,
+            task = "detect",
+            verbose = True,
+        )
+
+        for image_file in tqdm(os.listdir(images_path), desc="Images to process", unit="image"):
+             
+            _, file_ext = os.path.splitext(image_file)
+            image_path = os.path.join(images_path, image_file)
+           
+            if not os.path.isfile(image_path) or file_ext.lower() not in [".jpg", ".jpeg", ".png"]:
+                print(f"Skipping non-image file '{image_file}'.")
+                continue
+
+            results = model.predict(
+                source = image_path,
+                verbose = False,
+                conf = inference_config["conf"],
+                iou = inference_config["iou"],
+                device = "cpu",
+            )
+
+            if not results or len(results) == 0:
+                print(f"No results for image '{image_file}', skipping.")
+                continue
+
+            result = results[0]
+
+            boxes = result.boxes
+            
+            if not boxes:
+                print(f"No boxes detected for image '{image_file}', skipping.")
+                continue
+
+            class_ids = boxes.cls
+            confidences = boxes.conf
+            xyxy = boxes.xyxy
+            xywh = boxes.xywh
+
+            for class_id, confidence, xyxy, xywh in zip(class_ids, confidences, xyxy, xywh):
+
+                class_id = int(class_id)
+                confidence = float(confidence)
+                x1, y1, x2, y2 = map(float, xyxy)
+                x, y, w, h = map(float, xywh)
+
+                if image_file not in results_dict[model_name]:
+                    results_dict[model_name][image_path] = []
+
+                results_dict[model_name][image_path].append(
+                    {
+                        "class_id": class_id,
+                        "confidence": confidence,
+                        "xyxy": [x1, y1, x2, y2],
+                        "xywh": [x, y, w, h]
+                    }
+                )
+
+    with open(output_results_json, "w") as f:
+        json.dump(results_dict, f, indent=4)
+
+    print(f"Results saved to '{output_results_json}'.")
+
+
+    return
+
+if __name__ == "__main__":
+    main()
+        
+
